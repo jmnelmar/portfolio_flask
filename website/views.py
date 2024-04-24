895b21d088167c682f73494de5bd89420be81fda
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-04-24 15:57:56 -0700

    cleaning the repository

--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 views = Blueprint("views", __name__)
 
 @views.route('/')
 def home():
     return render_template("index.html")
+
+@views.route('/', methods=['POST'])
+def sent_email():
+    print("sent")
+    return redirect(url_for('views.home'))
