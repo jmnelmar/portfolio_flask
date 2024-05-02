a493676e175b795a86e58b689c86c70ea96b34ea
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-05-01 21:55:14 -0700

    adding sent email form

--- website/contact.py
+++ website/contact.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-
+from flask import Blueprint, render_template,flash, request, redirect, url_for
+import os, ezgmail
 contact = Blueprint("contact",__name__)
 
-@contact.route('/contact')
+_PATH = ".credentials/"
+
+@contact.route('/contact', methods=['POST'])
 def contact_route():
+    if request.method == "POST":
+        email = request.form.get("email")
+        subject = request.form.get("subject")
+        message = request.form.get("message")
+        
+        flash("OK")
     return render_template("contact.html")
+
+
+def send_email(email_address,subject , message):
+    os.chdir(_PATH)
+    ezgmail.init()
+    ezgmail.send('jmnel.mar@gmail.com','Test Subject','Testing messages sent by gmail 1')
